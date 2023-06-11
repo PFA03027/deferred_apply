b7f8d336ab49f205c489a84337a4a1895b897412
Alpha <PFA03027@nifty.com>
2023-06-11 18:16:25 +0900

    test: add test case

--- test/src/test_deferred_apply.cpp
+++ test/src/test_deferred_apply.cpp
@@ -175,7 +175,7 @@ TEST( Deferred_Apply, move_constructor )
 	EXPECT_TRUE( xx2.valid() );
 	EXPECT_EQ( 0, xx2.number_of_times_applied() );
 }
-#if 0
+
 TEST( Deferred_Apply, Do_move_constructor_with_move_only_parameter )
 {
 	// Arrange
@@ -190,16 +190,87 @@ TEST( Deferred_Apply, Do_move_constructor_with_move_only_parameter )
 	auto                 xx      = make_deferred_apply( &local::t_func, std::move( up_data ) );
 
 	// Act
-	// auto sut = std::move( xx );
-	// // decltype( xx ) sut = std::move( xx );
-	// auto ret = sut.apply();
+	auto sut = std::move( xx );
+	// decltype( xx ) sut = std::move( xx );
+	auto ret = sut.apply();
 
-	// // Assert
-	// EXPECT_EQ( std::type_index( typeid( up_data ) ), std::type_index( typeid( ret ) ) );
-	// EXPECT_EQ( p_addr, ret.get() );
-	// EXPECT_EQ( 5, ( *ret ) );
+	// Assert
+	EXPECT_EQ( std::type_index( typeid( up_data ) ), std::type_index( typeid( ret ) ) );
+	EXPECT_EQ( p_addr, ret.get() );
+	EXPECT_EQ( 5, ( *ret ) );
 }
-#endif
+
+TEST( Deferred_Apply, Do_copy_constructor_with_move_only_parameter_then_throw )
+{
+	// Arrange
+	struct local {
+		static std::unique_ptr<int> t_func( std::unique_ptr<int> arg )
+		{
+			return arg;
+		}
+	};
+	std::unique_ptr<int> up_data = std::unique_ptr<int>( new int( 5 ) );
+	auto                 xx      = make_deferred_apply( &local::t_func, std::move( up_data ) );
+
+	// Act
+	// Assert
+	ASSERT_THROW( {
+		auto sut = xx;
+	},
+	              std::bad_alloc );
+}
+
+TEST( Deferred_Apply, Do_move_constructor_with_copy_only_parameter_then_no_throw )
+{
+	// Arrange
+	struct local {
+		static void t_func( const local& arg )
+		{
+		}
+		local( void )         = default;
+		local( const local& ) = default;
+		local( local&& )      = delete;
+	};
+	local a;
+	auto  xx = make_deferred_apply( &local::t_func, a );
+	auto  yy = make_deferred_apply( &local::t_func, a );
+
+	// Act
+	// Assert
+	// xx, yyは、aへの参照を保持しているため、aの性質の影響を受けない。
+	ASSERT_NO_THROW( {
+		auto sut1 = xx;
+	} );
+	ASSERT_NO_THROW( {
+		auto sut2 = std::move( yy );
+	} );
+}
+
+TEST( Deferred_Apply, Do_copy_constructor_with_no_copy_move_parameter_then_no_throw )
+{
+	// Arrange
+	struct local {
+		static void t_func( const local& arg )
+		{
+		}
+		local( void )         = default;
+		local( const local& ) = delete;
+		local( local&& )      = delete;
+	};
+	local a;
+	auto  xx = make_deferred_apply( &local::t_func, a );
+	auto  yy = make_deferred_apply( &local::t_func, a );
+
+	// Act
+	// Assert
+	// xx, yyは、aへの参照を保持しているため、aの性質の影響を受けない。
+	ASSERT_NO_THROW( {
+		auto sut1 = xx;
+	} );
+	ASSERT_NO_THROW( {
+		auto sut2 = std::move( yy );
+	} );
+}
 
 TEST( Deferred_Apply, copy_assigner_from_empty_instance )
 {
